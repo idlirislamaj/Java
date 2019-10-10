cc05d45bdd8aaa3a21cc7175b7ece4107c56f57e
Idlir Islamaj <idlirislamaj@gmail.com>
2019-10-10 19:49:10 +0200

    added fixes on finding smallestNumber when two 00

--- SecondSmallest.java
+++ SecondSmallest.java
@@ -1,18 +1,19 @@
+
 //This simple program finds second smallest of a given array
-public class SecondSmallest {
-    public static void main(String args[]) {
-      int [] elements = {12, 34, 54, 33, 75,0, 7, 6, 88};
-      int smallestNumber = elements[0];
-      int secondSmallestNumber = elements[0];
-      for(int i=0; i < elements.length; i++ ){
-          if(smallestNumber > elements[i]){
-              secondSmallestNumber = smallestNumber;
-              smallestNumber = elements[i];
+    public class SecondSmallest {
+        public static void main(String args[]) {
+          int [] elements = {12, 34, 54, 33, 75,0, 0, 6, 88};
+          int smallestNumber = Integer.MAX_VALUE;
+          int secondSmallestNumber = Integer.MAX_VALUE;
+
+          for(int i=0; i < elements.length; i++ ){
+              if(elements[i] < smallestNumber){
+                  smallestNumber = elements[i];
+              }
+              else if(elements[i] > smallestNumber && elements[i] < secondSmallestNumber){
+                  secondSmallestNumber = elements[i];
+              }
           }
-          else if(secondSmallestNumber > elements[i]){
-              secondSmallestNumber = elements[i];
-          }
-      }
-      System.out.println(secondSmallestNumber);
-    }
- }
+          System.out.println(secondSmallestNumber);
+        }
+     }
