4d857b47fd5b144ed57dc26ad8584975d7d00b0f
Idlir Islamaj <idlirislamaj@gmail.com>
2019-10-13 17:15:18 +0200

    fixes on secondLargest to get the second largest number and skip identical numbers

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
 // This simple program finds second largest number of a given array
 public class SecondLargest {
     public static void main(String args[]) {
-      int [] elements = {12, 34, 54, 33, 75,0, 7, 6, 88};
-      int smallestNumber = elements[0];
-      int secondSmallestNumber = elements[0];
-      for(int i=0; i < elements.length; i++ ){
-          if(smallestNumber < elements[i]){
-              secondSmallestNumber = smallestNumber;
-              smallestNumber = elements[i];
-          }
-          else if(secondSmallestNumber < elements[i]){
-              secondSmallestNumber = elements[i];
-          }
+        int [] elements = {12, 34, 54, 33, 75,0, 0, 6, 88, 77 ,12, 559, 88, 559};
+        int largestNumber = Integer.MIN_VALUE;
+        int secondLargestNumber = Integer.MIN_VALUE;
+
+        for(int i=0; i < elements.length; i++ ){
+            if(elements[i] > largestNumber){
+                secondLargestNumber = largestNumber;
+                largestNumber = elements[i];
+            }
+            else if(elements[i] > secondLargestNumber && elements[i] < largestNumber){
+                secondLargestNumber = elements[i];
+            }
+        }
+        System.out.println("Second Smallest number of the array "+ Arrays.toString(elements)+ " is "  +secondLargestNumber);
       }
-      System.out.println(secondSmallestNumber);
-    }
- }
+   }
