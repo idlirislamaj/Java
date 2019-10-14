f55342d67cc3191accc37f703ca4f5a52e4bca5a
Idlir Islamaj <idlirislamaj@gmail.com>
2019-10-14 18:54:56 +0200

    deleted a line on SecondLargest not needed

--- SecondLargest.java
+++ SecondLargest.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 // This simple program finds second largest number of a given array
 public class SecondLargest {
     public static void main(String args[]) {
-        int [] elements = {12, 34, 54, 33, 75,0, 0, 6, 88, 77 ,12, 559, 88, 559};
+        int [] elements = {88, 559, 54, 33, 75,88, 0, 6, 559, 77 ,12, 559, 88, 88};
         int largestNumber = Integer.MIN_VALUE;
         int secondLargestNumber = Integer.MIN_VALUE;
 
         for(int i=0; i < elements.length; i++ ){
             if(elements[i] > largestNumber){
-                secondLargestNumber = largestNumber;
                 largestNumber = elements[i];
             }
             else if(elements[i] > secondLargestNumber && elements[i] < largestNumber){
