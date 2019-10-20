8d11a5c147f6a3063673b7fd9bd0838332765372
Idlir Islamaj <idlirislamaj@gmail.com>
2019-10-20 17:56:44 +0200

    Added a program that checks duplicates from User input and shows them

---
+++ RemoveDuplicates.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        List<String> wordsArray = new ArrayList<>();
+        List<String> duplicatedWordsArray = new ArrayList<>();
+        System.out.println("Enter words to the array");
+        Scanner input = new Scanner(System.in);
+        String words;
+
+        do {
+            words = input.nextLine();
+            if (wordsArray.contains(words)){
+                duplicatedWordsArray.add(words);
+            } else if(words.equals("")){
+                    break;
+            }
+            else {
+                wordsArray.add(words);
+
+            }
+        }while (!words.equals(""));
+
+        System.out.println("Here are the elements entered to the array "+wordsArray);
+        System.out.println("Here are the duplicates found "+duplicatedWordsArray);
+
+
+    }
+}
