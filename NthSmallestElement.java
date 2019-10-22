09ea2a467d16bdbcc824fcab83d82b12372f8457
Idlir Islamaj <idlirislamaj@gmail.com>
2019-10-22 21:28:44 +0200

    Added a program with an internal method that sorts than finds the nth element of Set collection and returns it. if the collection is smaller then returns Integer.MAX_VALUE

---
+++ NthSmallestElement.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class NthSmallestElement {
+
+    public static void main(String[] args) {
+
+        List<Integer> myList = List.of(10,15,40,50,60,70,30,5,8,24);
+        Set<Integer> mySet = new HashSet<>(myList);
+
+
+        System.out.println(nthSmallestElement(mySet, 7));
+
+
+
+    }
+
+
+    public static Integer nthSmallestElement(Set<Integer> mySet, int nth){
+
+        Set<Integer> treeSet = new TreeSet<>(mySet);
+        int i = 0;
+        for (Integer myInt : treeSet){
+            i++;
+            if (i == nth){
+                return myInt;
+            }
+
+            else if(treeSet.size()< nth){
+                return Integer.MAX_VALUE;
+            }
+        }
+        return null;
+    }
+}
